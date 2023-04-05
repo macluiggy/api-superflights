a63922045cb49aca2f9c9043bd710bb625b7262b
luiggy macias <ferrinluiggy@gmail.com>
2023-04-04 21:35:09 -0500

    agregando modulo y demas para user

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class UserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+}
