47ac88862827d57ae0f2b5e39b4610bda07537ae
luiggy macias <ferrinluiggy@gmail.com>
2023-04-09 22:08:21 -0500

    controller and service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+import { IUser } from 'src/common/interfaces/user.interface';
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { USER } from 'src/common/models/models';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(USER.name) private readonly model: Model<IUser>) {}
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+  async create(userDto: UserDto): Promise<IUser> {
+    const hash = await this.hashPassword(userDto.password);
+    const newUser = new this.model({ ...userDto, password: hash });
+    return await newUser.save();
+  }
+}
