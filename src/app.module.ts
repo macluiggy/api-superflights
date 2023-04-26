d4122e8de9cabe38c56b534b33bdce32556a59f0
macluiggy <luiggy.macias@imagineapps.co>
2023-04-25 20:35:03 -0500

    auth and env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { PassengerModule } from './passenger/passenger.module';
 import { FlightModule } from './flight/flight.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { FlightModule } from './flight/flight.module';
     UserModule,
     PassengerModule,
     FlightModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
