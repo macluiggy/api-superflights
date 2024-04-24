06e1b7954df09acf02603d7979fdcaf00efce34c
macluiggy <luiggy.macias@imagineapps.co>
2024-04-23 19:40:49 -0500

    Fix formatting in HttpExceptionFilter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -17,12 +17,14 @@ export class HttpExceptionFilter implements ExceptionFilter {
       ? exception.getStatus()
       : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const msg = exception.getResponse ? exception.getResponse() : exception.message;
+    const msg = exception.getResponse
+      ? exception.getResponse()
+      : exception.message;
     console.error(exception);
-    
+
     this.logger.error(
       `Http Status: ${status}, Error Message: ${JSON.stringify(msg)}`,
-    )
+    );
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
